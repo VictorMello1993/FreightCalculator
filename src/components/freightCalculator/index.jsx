dc1a8c3e4426d2c088414ffa6ad451d8a284e46a
Victor Mello <victor.mello@al.infnet.edu.br>
2022-02-24 20:28:26 -0300

    Implementação dos combos de estados e cidades e CEP do remetente e destinatário

--- src/components/freightCalculator/index.jsx
+++ src/components/freightCalculator/index.jsx
@@ -2,29 +2,30 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Input, InputGroup } from 'reactstrap';
 import styled from 'styled-components';
+import InputMask from 'react-input-mask';
 
 const FreightCalculator = () => {
 
-  const [categorias, setCategoria] = useState([])
-  const [produtos, setProduto] = useState([])
+  const [estados, setEstado] = useState([])
+  const [cidades, setCidade] = useState([])
 
-  const fazerChamadaAPI = async () => {    
-    const categorias = await axios.get('http://localhost:3001/Categorias');    
-    setCategoria(categorias.data);
+  const fazerChamadaAPI = async () => {
+    const estados = await axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados');
+    setEstado(estados.data);
   }
 
   useEffect(() => {
     fazerChamadaAPI();
   }, []);
 
-  const handleChangeCategorias = async (event) => {
-    const idCategoria = event.target.value
-    console.log(idCategoria)
-    const produtosPorCategoria = await axios.get(
-      `http://localhost:3001/Produtos?categoriaId=${idCategoria}`
-    );
+  const handleChangeEstado = async (event) => {
+    const idEstado = event.target.value    
 
-    setProduto(produtosPorCategoria.data);
+    const cidadesPorEstado = await axios.get(
+      `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${idEstado}/municipios`
+    );
+        
+    setCidade(cidadesPorEstado.data);
   }
 
   return (
@@ -33,52 +34,82 @@ const FreightCalculator = () => {
         Preencha os dados aqui
       </Title>
       <Form>
-        <FormGroup>
-          <Input placeholder="username" />
-          <Input placeholder="username" />
-          <Input placeholder="username" />
-        </FormGroup>
-        <FormGroup>          
-          <Input
-            id="exampleSelect"
-            name="categorias"
-            type="select"
-            onChange={handleChangeCategorias}
-          >
-            <option value="">--categorias---</option>
-            {categorias.map((cat, i) => (
-              <option key={i} value={cat.id}>
-                {cat.body}
+        <FieldSet>
+          --Remetente--
+          <FormGroup>
+            <Input placeholder="Estado (UF)" 
+                  type="select" 
+                  name="estados" 
+                  id="selectEstados"
+                  className="input" 
+                  onChange={handleChangeEstado}>
+              <option value="">--Estado (UF)--</option>
+              {estados.map((est, i) => (
+              <option key={i} value={est.id}>
+                {est.nome} ({est.sigla})
               </option>
-            ))} 
-          </Input>
-          <Input
-            id="exampleSelect"
-            name="produtos"
-            type="select"
-          >
-            <option value="">-- produtos --</option>
-            {produtos.map((prod, i) => (
-              <option key={i} value={prod.id}>
-                {prod.body}
+            ))}
+            </Input>
+            <Input placeholder="Cidade" type="select" name="cidades" id="selectCidades" className="input">
+              <option value="">--Cidade--</option>
+              {cidades.map((cid, i) => (
+              <option key={i} value={cid.id}>
+                {cid.nome}
               </option>
-            ))}            
+            ))}
+            </Input>     
+            <Input type="tel" placeholder="CEP" mask="99999-999" maskChar="" tag={InputMask}/>
+          </FormGroup>
+        </FieldSet>
+        <FieldSet>
+          --Destinatário--
+          <FormGroup>
+            <Input placeholder="Estado (UF)" 
+                  type="select" 
+                  name="estados" 
+                  id="selectEstados"
+                  className="input" 
+                  onChange={handleChangeEstado}>
+              <option value="">--Estado (UF)--</option>
+              {estados.map((est, i) => (
+              <option key={i} value={est.id}>
+                {est.nome} ({est.sigla})
+              </option>
+            ))}
+            </Input>
+            <Input placeholder="Cidade" type="select" name="cidades" id="selectCidades" className="input">
+              <option value="">--Cidade--</option>
+              {cidades.map((cid, i) => (
+              <option key={i} value={cid.id}>
+                {cid.nome}
+              </option>
+            ))}
+            </Input>     
+            <Input type="tel" placeholder="CEP" mask="99999-999" maskChar="" tag={InputMask}/>
+          </FormGroup>
+        </FieldSet>        
+        <FieldSet>
+          --Informações da encomenda--
+          <FormGroup>
+          <Input type="number" placeholder="peso (KG)" min="0">
           </Input>
-        </FormGroup>
+          </FormGroup>
+        </FieldSet>      
       </Form>
     </FormContainer>
   )
 }
 
 const Form = styled.div`
-  padding: 20px;
+  padding: 20px;  
 `
 
-const Title = styled.p`
-  font-size: 25px;
+const Title = styled.div`
+  font-size: 25px;  
   font-weight: 600;
   text-align: center;    
   color: #335185;    
+  padding: 15px;
 `
 
 const FormContainer = styled.div`
@@ -88,7 +119,13 @@ const FormContainer = styled.div`
 
 const FormGroup = styled(InputGroup)`    
   gap: 10px;
-  margin-bottom: 20px;
+  margin: 10px 0;  
+`
+
+const FieldSet = styled.fieldset`
+  color: #335185;
+  font-weight: 600;  
+  padding: 15px;
 `
 
 export default FreightCalculator
