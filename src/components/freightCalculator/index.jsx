fa42583f7c831001c9da2c81059c780209bd36f8
Victor Mello <victor.mello@al.infnet.edu.br>
2022-02-24 23:10:33 -0300

    Adicionados campos de informações da encomenda (peso, formato, altura, largura, diâmetro) e tipo de serviço

--- src/components/freightCalculator/index.jsx
+++ src/components/freightCalculator/index.jsx
@@ -8,10 +8,14 @@ const FreightCalculator = () => {
 
   const [estados, setEstado] = useState([])
   const [cidades, setCidade] = useState([])
+  const [servicos, setServico] = useState([])
 
   const fazerChamadaAPI = async () => {
     const estados = await axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados');
+    const servicos = await axios.get('http://localhost:3001/servicos');
+
     setEstado(estados.data);
+    setServico(servicos.data);
   }
 
   useEffect(() => {
@@ -19,14 +23,16 @@ const FreightCalculator = () => {
   }, []);
 
   const handleChangeEstado = async (event) => {
-    const idEstado = event.target.value    
+    const idEstado = event.target.value
 
     const cidadesPorEstado = await axios.get(
       `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${idEstado}/municipios`
     );
-        
+
     setCidade(cidadesPorEstado.data);
   }
+
+  const blockInvalidChar = event => ['e', 'E', '+', '-'].includes(event.key) && event.preventDefault();
 
   return (
     <FormContainer>
@@ -37,64 +43,101 @@ const FreightCalculator = () => {
         <FieldSet>
           --Remetente--
           <FormGroup>
-            <Input placeholder="Estado (UF)" 
-                  type="select" 
-                  name="estados" 
-                  id="selectEstados"
-                  className="input" 
-                  onChange={handleChangeEstado}>
+            <Input placeholder="Estado (UF)"
+              type="select"
+              name="estados"
+              id="selectEstados"
+              onChange={handleChangeEstado}>
               <option value="">--Estado (UF)--</option>
               {estados.map((est, i) => (
-              <option key={i} value={est.id}>
-                {est.nome} ({est.sigla})
-              </option>
-            ))}
+                <option key={i} value={est.id}>
+                  {est.nome} ({est.sigla})
+                </option>
+              ))}
             </Input>
-            <Input placeholder="Cidade" type="select" name="cidades" id="selectCidades" className="input">
+            <Input placeholder="Cidade" type="select" name="cidades" id="selectCidades">
               <option value="">--Cidade--</option>
               {cidades.map((cid, i) => (
-              <option key={i} value={cid.id}>
-                {cid.nome}
-              </option>
-            ))}
-            </Input>     
-            <Input type="tel" placeholder="CEP" mask="99999-999" maskChar="" tag={InputMask}/>
+                <option key={i} value={cid.id}>
+                  {cid.nome}
+                </option>
+              ))}
+            </Input>
+            <Input type="tel" placeholder="CEP" mask="99999-999" maskChar="" tag={InputMask} />
           </FormGroup>
         </FieldSet>
         <FieldSet>
           --Destinatário--
           <FormGroup>
-            <Input placeholder="Estado (UF)" 
-                  type="select" 
-                  name="estados" 
-                  id="selectEstados"
-                  className="input" 
-                  onChange={handleChangeEstado}>
-              <option value="">--Estado (UF)--</option>
+            <Input placeholder="Estado (UF)"
+              type="select"
+              name="estados"
+              id="selectEstados"
+              onChange={handleChangeEstado}>
+              <option>--Estado (UF)--</option>
               {estados.map((est, i) => (
-              <option key={i} value={est.id}>
-                {est.nome} ({est.sigla})
-              </option>
-            ))}
+                <option key={i} value={est.id}>
+                  {est.nome} ({est.sigla})
+                </option>
+              ))}
             </Input>
-            <Input placeholder="Cidade" type="select" name="cidades" id="selectCidades" className="input">
-              <option value="">--Cidade--</option>
+            <Input placeholder="Cidade" type="select" name="cidades" id="selectCidades">
+              <option>--Cidade--</option>
               {cidades.map((cid, i) => (
-              <option key={i} value={cid.id}>
-                {cid.nome}
-              </option>
-            ))}
-            </Input>     
-            <Input type="tel" placeholder="CEP" mask="99999-999" maskChar="" tag={InputMask}/>
+                <option key={i} value={cid.id}>
+                  {cid.nome}
+                </option>
+              ))}
+            </Input>
+            <Input type="tel" placeholder="CEP" mask="99999-999" maskChar="" tag={InputMask} />
           </FormGroup>
-        </FieldSet>        
+        </FieldSet>
         <FieldSet>
           --Informações da encomenda--
           <FormGroup>
-          <Input type="number" placeholder="peso (KG)" min="0">
-          </Input>
+            <Input type="number"
+              placeholder="Peso (kg)"
+              min="0"
+              onKeyDown={blockInvalidChar} />
+            <Input type="select" placeholder="Formato" id="selectFormatos">
+              <option>--Formato--</option>
+              <option value="1">Caixa/Pacote</option>
+              <option value="2">Rolo/Prisma</option>
+              <option value="3">Envelope</option>
+            </Input>
+            <Input type="number"
+              placeholder="Comprimento (cm)"
+              min="0"
+              onKeyDown={blockInvalidChar} />
+
+            <Input type="number"
+              placeholder="Altura (cm)"
+              min="0"
+              onKeyDown={blockInvalidChar} />
+            <Input type="number"
+              placeholder="Largura (cm)"
+              min="0"
+              onKeyDown={blockInvalidChar} />
+            <Input type="number"
+              placeholder="Diâmetro (cm)"
+              min="0"
+              onKeyDown={blockInvalidChar} />
           </FormGroup>
-        </FieldSet>      
+        </FieldSet>
+        <hr />
+        <FormGroup>
+          <Input type="select"
+            min="0"            
+            name="servicos"
+            id="selectServicos">
+            <option>--Tipo de serviço--</option>
+            {servicos.map((svc, i) => (
+              <option key={i} value={svc.id}>
+                {svc.descricao}
+              </option>
+            ))}
+          </Input>
+        </FormGroup>
       </Form>
     </FormContainer>
   )
@@ -119,7 +162,11 @@ const FormContainer = styled.div`
 
 const FormGroup = styled(InputGroup)`    
   gap: 10px;
-  margin: 10px 0;  
+  margin: 10px 0;
+
+  #selectServicos{
+    max-width: 30%;
+  }
 `
 
 const FieldSet = styled.fieldset`
