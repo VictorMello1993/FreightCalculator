6d057fac65bc6dbbb80e29d6a9c054c9c6b82026
Victor Mello <victor.mello@al.infnet.edu.br>
2022-03-23 08:11:27 -0300

    Ocultando secret key com dotenv

--- backend/utils/helpers.js
+++ backend/utils/helpers.js
@@ -1,7 +1,11 @@
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
 const JWT_EXPIRES_IN = 60 //1min
-const SECRET_KEY = '0ff66555f7028c5da372fbf8628939b1'
+const {SECRET_KEY: secret} = process.env;
 
 exports.generateHash = async (password) => {
   const passEncode = await bcrypt.genSaltSync(10, 'a')
@@ -11,14 +15,14 @@ exports.generateHash = async (password) => {
 exports.generateToken = (payload) => {
   return {
     type: 'Bearer',
-    token: jwt.sign({...payload}, SECRET_KEY, {
+    token: jwt.sign({...payload}, secret, {
       expiresIn: parseInt(JWT_EXPIRES_IN)
     })
   }
 }
 
 exports.verifyToken = (token) => 
-  jwt.verify(token, SECRET_KEY, (err, data) => {        
+  jwt.verify(token, secret, (err, data) => {        
     if(err){            
       return false
     }
