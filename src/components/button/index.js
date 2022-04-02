fc4d5e60d14399f152391bfccece6b219bda211a
Victor Mello <victor.mello@al.infnet.edu.br>
2022-04-02 17:29:11 -0300

    Testando a API do Strapi

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { Button as ButtonBootstrap, Spinner } from 'reactstrap';
 
 export const Button = ({ children, loading = false, ...otheProps } = {}) => {
-  console.log('otheProps', otheProps)
   return (
     <ButtonStyled {...otheProps}>
       
--- src/services/auth/auth.service.js
+++ src/services/auth/auth.service.js
@@ -1,3 +1,8 @@
 import http from './../../config/http';
 
-export const authenticatedUser = (values) => http.post('/login', values)
+// export const authenticatedUser = (values) => http.post('/login', values)
+export const authenticatedUser = (values) => http.post('/auth/local', values)
+
+  
+
+  
