59df230c7ec374d7b0eed7a6556f48e3407cec31
Victor Mello <victor.mello@al.infnet.edu.br>
2022-04-16 18:26:21 -0300

    Ajustes finais

--- backend/middlewares/ensureAuthenticated.js
+++ backend/middlewares/ensureAuthenticated.js
@@ -4,24 +4,21 @@ exports.ensureAuthenticated = async(req, res, next) => {
   const authHeader = req.headers.authorization
 
   if (!authHeader) {
-    res.status(401).json('Usuário não está autorizado')
-    return
+    return res.status(401).json('Usuário não está autorizado')    
   }
 
   const parts = authHeader?.split(' ')
   if (parts?.length !== 2) {
-    res.status(401).json('Tipo de autorização inválida')
-    return
+    return res.status(401).json('Tipo de autorização inválida')    
   }
 
   const [schema, token] = parts
   if (schema?.toLowerCase() !== 'bearer') {
-    res.status(401).json('Token mal formatado')
+    return res.status(401).json('Token mal formatado')
   }
   
   if (!verifyToken(token)) {
-    res.status(401).json('Token inválido')
-    return
+    return res.status(401).json('Token inválido')    
   }
 
   next()
